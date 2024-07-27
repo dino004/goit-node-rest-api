1f4509d9e758923a01c65325154ff2b71faf2917
dino004 <dmytro4lytvyn@gmail.com>
2024-07-27 16:26:16 +0300

    create database

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+import mongoose from "mongoose";
+
 import app from "./app.js";
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Dmytro:VQ6qozqF6wPZjWMW@cluster0.dwbfnbc.mongodb.net/db-contacts?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// VQ6qozqF6wPZjWMW
